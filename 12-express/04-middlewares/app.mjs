ed28c2237bbd8494f0cb645d6f2f3b168c44a2c9
Anna Kudriashova <amarenoru@gmail.com>
2023-11-15 10:27:07 +0200

    frontend and backend express

--- 12-express/04-middlewares/app.mjs
+++ 12-express/04-middlewares/app.mjs
@@ -1,12 +1,42 @@
 import express from 'express';
 import morgan from 'morgan';
+// import qs from 'querystring';
 
 const app = express();
 // logs info about request
 app.use(morgan('tiny'));
+
 // converts JSON to JS Object in POST, PUT, PATCH request
+
+// app.use((req, res, next) => {
+//     let data = '';
+//     req.on('data', (chunk) => (data += chunk));
+//     req.on('end', () => {
+//         const parsedJSON = JSON.parse(data);
+//         req.body = parsedJSON;
+//         next();
+//     });
+// });
+
+// converts JSON to JS Object in POST, PUT, PATCH request with method express.json()
 app.use(express.json());
-// conerts from data to JS Object in POST, PUT, PATCH request
+
+// converts form data to JS Object in POST, PUT, PATCH request
+// app.use((req, res, next) => {
+//     if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+//         let data = '';
+//         req.on('data', (chunk) => (data += chunk.toString()));
+//         req.on('end', () => {
+//             const parsedFormData = qs.parse(data);
+//             req.body = parsedFormData;
+//             next();
+//         });
+//     } else {
+//         next();
+//     }
+// });
+
+// covnerts form data to JS Object in POST, PUT, PATCH request with method express.urlencoded()
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res) => {
